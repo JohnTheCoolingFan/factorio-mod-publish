697b9af8a8d41088fc00dbf61b37c00e9e4dc965
JohnTheCoolingFan <ivan8215145640@gmail.com>
2024-07-14 16:37:33 +0300

    Reduce complexity

--- publish.sh
+++ publish.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-if [[ -v INFO_JSON_FILE ]]; then
-    export MOD_NAME="$(jq -r .name $INFO_JSON_FILE)"
-    if [[ ! -v MOD_FILE ]]; then
-        export MOD_FILE=$(jq -r '.name + "_" + .version + ".zip"' $INFO_JSON_FILE)
-    fi
-fi
-
-if [[ ! -v MOD_NAME ]]; then
-    echo "MOD_NAME is not set"
-    exit 1
-fi
-
-if [[ ! -v MOD_FILE ]] || [[ -z MOD_FILE ]]; then
-    echo "MOD_FILE is not set"
-    exit 1
-fi
+export MOD_NAME="$(jq -r .name $INFO_JSON_FILE)"
+export MOD_FILE=$(jq -r '.name + "_" + .version + ".zip"' $INFO_JSON_FILE)
 
 UPLOAD_URL=$(curl -X POST -d mod="${MOD_NAME}" -H "Authorization: Bearer ${FACTORIO_PORTAL_TOKEN}"  https://mods.factorio.com/api/v2/mods/releases/init_upload | jq -r ".upload_url")
 
