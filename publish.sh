2ad72fb6768bfce9e44120a0e48e4e9dbbdf1b8a
JohnTheCoolingFan <ivan8215145640@gmail.com>
2024-07-14 16:41:33 +0300

    Fix curl syntax

--- publish.sh
+++ publish.sh
@@ -5,4 +5,4 @@ export MOD_FILE=$(jq -r '.name + "_" + .version + ".zip"' $INFO_JSON_FILE)
 
 UPLOAD_URL=$(curl -X POST -d mod="${MOD_NAME}" -H "Authorization: Bearer ${FACTORIO_PORTAL_TOKEN}"  https://mods.factorio.com/api/v2/mods/releases/init_upload | jq -r ".upload_url")
 
-curl -X POST -s -F "file=${MOD_FILE}" "${UPLOAD_URL}"
+curl -X POST -s -F "file=@${MOD_FILE}" "${UPLOAD_URL}"
